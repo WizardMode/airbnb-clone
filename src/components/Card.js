e2c3f688061461d90999a2b4ea3cd72d74a8a223
WizardMode <humanspacebolt@live.com>
2022-03-29 14:46:00 -0700

    conditional rendering

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,14 +2,21 @@ import React from "react";
 import star from "../images/Star.png";
 
 function Card(props) {
+    let badgeText
+    if (props.openSpots === 0) {
+        badgeText = "SOLD OUT"
+    } else if (props.location === "Online") {
+        badgeText = "ONLINE"
+    }
     return (
         <div className="card">
+            {badgeText && <div className="card-badge">{badgeText}</div>}
             <img src={props.img} className="card-image" />
             <div className="card-stats">
                 <img src={star} className="card-star" />
                 <span>{props.rating}</span>
                 <span className="gray"> ({props.reviewCount}) * </span>
-                <span className="gray">{props.country}</span>
+                <span className="gray">{props.location}</span>
             </div>
             <p className="card-title">{props.title}</p>
             <p className="card-price"><span className="bold">From {props.price}$</span> / person</p>
