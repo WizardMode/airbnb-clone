fb3d3ae506d64cdfc983b5387407a8da873e6a99
WizardMode <humanspacebolt@live.com>
2022-03-28 18:24:24 -0700

    Card component with props

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,19 +1,18 @@
 import React from "react";
-import photo from "../images/katie-zaferes.png";
 import star from "../images/Star.png";
 
 function Card(props) {
     return (
         <div className="card">
-            <img src={photo} className="card-image" />
+            <img src={props.img} className="card-image" />
             <div className="card-stats">
                 <img src={star} className="card-star" />
-                <span>5.0</span>
-                <span className="gray">(6) * </span>
-                <span className="gray">USA</span>
+                <span>{props.rating}</span>
+                <span className="gray"> ({props.reviewCount}) * </span>
+                <span className="gray">{props.country}</span>
             </div>
-            <p>Life Lessons with Katie Zaferes</p>
-            <p><span className="bold">From 136$</span> / person</p>
+            <p className="card-title">{props.title}</p>
+            <p className="card-price"><span className="bold">From {props.price}$</span> / person</p>
         </div>
     )
 }
